60d86e96798b8db806eb3f55b3adcd3397dc687a
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-06-23 13:23:19 -0600

    Fix development server

--- app/frontend/entrypoints/application.jsx
+++ app/frontend/entrypoints/application.jsx
@@ -5,7 +5,13 @@ import "../styles/application.css"
 createInertiaApp({
   resolve: (name) => {
     const pages = import.meta.glob("../pages/**/*.jsx", { eager: true })
-    return pages[`../pages/${name}.jsx`]
+    const page = pages[`../pages/${name}.jsx`]
+
+    if (!page) {
+      throw new Error(`Missing Inertia page: ${name}`)
+    }
+
+    return page.default
   },
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
--- config/initializers/inertia_rails.rb
+++ config/initializers/inertia_rails.rb
@@ -1,3 +1,4 @@
 InertiaRails.configure do |config|
   config.version = ViteRuby.digest
+  config.use_script_element_for_initial_page = true
 end
